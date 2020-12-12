e993daad1293a1186b1c7265c57079ff9c4b1c70
ebwb <ebwbernard@gmail.com>
2020-12-12 01:37:08 -0600

    Format file. Add search functionality, along with basic unit test.

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,8 @@
+use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
-use std::error::Error;
 
-
-#[derive(PartialEq)]
-#[derive(Debug)]
+#[derive(PartialEq, Debug)]
 pub struct Config {
     pub query: String,
     pub filename: String,
@@ -12,14 +10,14 @@ pub struct Config {
 
 impl Config {
     pub fn new(args: &[String]) -> Result<Config, &'static str> {
-	if args.len() < 3 {
-	    return Err("not enough arguments");
-	}
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
 
-	let query = args[1].clone();
-	let filename = args[2].clone();
-	
-	Ok(Config {query, filename})
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config { query, filename })
     }
 }
 
@@ -34,49 +32,72 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-// disabling snake case for more legible tes names
+
+// The return value is a collection of references of str slices; therefore, the references within the collection must have the same lifetime as the input contents.
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    
+    for line in contents.lines() {
+	if line.contains(query) {
+	    results.push(line);
+	}
+    }
+
+    results
+}
+
+// disabling snake case for more legible test > names
 #[allow(non_snake_case)]
 #[cfg(test)]
 mod tests {
     use super::*;
-    
+
     #[test]
     fn one_result() {
-
+        let query = "duct";
+        let contents = "\
+	Rust:
+safe, fast, productive.
+Pick three.";
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
 
     #[test]
     fn fewerThan3Args_returnsErr() {
-	let not_enough_args : Vec<String> = vec![String::from("test")];
-	
-	if let Ok(config) = Config::new(&not_enough_args) {
-	    panic!("this should not have been Ok(()): {:?}", config);
-	}
+        let not_enough_args: Vec<String> = vec![String::from("test")];
+
+        if let Ok(config) = Config::new(&not_enough_args) {
+            panic!("this should not have been Ok(()): {:?}", config);
+        }
     }
 
     #[test]
     fn threeArgs_returnsConfig() {
-	let arg1 = String::from("arg1");
-	let arg2 = String::from("arg2");
-	let arg3 = String::from("arg3");
+        let arg1 = String::from("arg1");
+        let arg2 = String::from("arg2");
+        let arg3 = String::from("arg3");
 
-	let just_right_args : Vec<String> = vec![arg1, arg2, arg3];
+        let just_right_args: Vec<String> = vec![arg1, arg2, arg3];
 
-	if let Err(e) = Config::new(&just_right_args) {
-	    panic!("this should have been Ok(()): {}", e);
-	}
+        if let Err(e) = Config::new(&just_right_args) {
+            panic!("this should have been Ok(()): {}", e);
+        }
     }
 
     #[test]
     fn newConfig_usesSecondAndThirdArgs() {
-	let arg1 = String::from("arg1");
-	let arg2 = String::from("arg2");
-	let arg3 = String::from("arg3");
+        let arg1 = String::from("arg1");
+        let arg2 = String::from("arg2");
+        let arg3 = String::from("arg3");
 
-	let just_right_args : Vec<String> = vec![arg1, arg2.clone(), arg3.clone()];
+        let just_right_args: Vec<String> = vec![arg1, arg2.clone(), arg3.clone()];
 
-	assert_eq!(
-	    Config {query: arg2, filename: arg3},
-	    Config::new(&just_right_args).unwrap());
+        assert_eq!(
+            Config {
+                query: arg2,
+                filename: arg3
+            },
+            Config::new(&just_right_args).unwrap()
+        );
     }
 }
